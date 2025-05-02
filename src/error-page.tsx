edb663a219df71adbcd9f9c512e87e82675524e6
Sumit Saini <01sumitsaini@gmail.com>
2025-05-02 17:09:58 +0530

    error page updated

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -2,6 +2,20 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
     const error = useRouteError();
+
+    let errorMessage: string = "An unexpected error has occurred.";
+
+    if (error instanceof Error) {
+        errorMessage = error.message;
+    } else if (
+        typeof error === "object" &&
+        error !== null &&
+        "statusText" in error &&
+        typeof (error as any).statusText === "string"
+    ) {
+        errorMessage = (error as any).statusText;
+    }
+
     console.error(error);
 
     return (
@@ -9,7 +23,7 @@ export default function ErrorPage() {
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.statusText || error.message}</i>
+                <i>{errorMessage}</i>
             </p>
         </div>
     );
